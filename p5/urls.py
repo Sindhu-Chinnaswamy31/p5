d7bcd5491eb25419c1071495d71abad685b68e3d
Your Name <sindhucswamy@gmail.com>
2020-08-11 18:28:08 +0530

    first commit

--- p5/urls.py
+++ p5/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('myapp/',include("myapp.urls")),
     path('get_demo/',views.get_demo,name="get_demo"),
     path('post_demo/',views.post_demo,name="post_demo"),
+    path('register/',views.register,name="register"),
 ]
